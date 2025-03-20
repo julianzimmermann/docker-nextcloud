9ca2263f75190635d186908a5f37ccbffcc44f42
Julian Hyatt <dev@jzit.de>
2025-03-20 10:10:36 +0100

    chore: cleanup

--- Docker/conf/php-cli/pm2/ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apps: [{
-        name: "nextcloud-cron",
-        script: "/var/www/nextcloud/releases/current/cron.php --define apc.enable_cli=1",
-        cron_restart: "*/5  *  *  *  *",
-        autorestart: false,
-        watch: true
-    }]
-}
--- Docker/conf/php-fpm/pm2/ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apps: [{
-        name: "nextcloud-cron",
-        script: "/var/www/nextcloud/releases/current/cron.php",
-        cron_restart: "*/5  *  *  *  *",
-        autorestart: false,
-        watch: true
-    }]
-}
